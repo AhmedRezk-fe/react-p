9e334f1b3a1bc113b03cbb88794a1db822beb1e2
Ahmed Rizk <arizk456@gmail.com>
2019-07-27 21:50:35 +0200

    Updates

---
+++ precache-manifest.63f181aa0ddd8cfdff4568bf7b60cbf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4d1fad0073e5d58e849ec8d2bb26c7",
+    "url": "/react-p/index.html"
+  },
+  {
+    "revision": "a328fbb7835548351949",
+    "url": "/react-p/static/css/main.fc71d57d.chunk.css"
+  },
+  {
+    "revision": "49cf1230ce9c874af46e",
+    "url": "/react-p/static/js/2.2d27a041.chunk.js"
+  },
+  {
+    "revision": "a328fbb7835548351949",
+    "url": "/react-p/static/js/main.1b06c372.chunk.js"
+  },
+  {
+    "revision": "611ee4ce5ab5369d5b23",
+    "url": "/react-p/static/js/runtime~main.aabd0bc4.js"
+  }
+]);
